80059e7b0cf1785c49698074b6eb6628407e2cfa
Seung-wan <tmddhks0104@gmail.com>
2024-08-05 21:41:11 +0900

    useForm의 init, getFields를 작성합니다.

--- useForm.ts
+++ useForm.ts
@@ -1,8 +1,43 @@
-function useForm() {
-  const init = () => {};
+import { createRef, useState } from 'react';
+import type { RefObject } from 'react';
+
+type InputRefs = RefObject<HTMLInputElement>;
+
+type State = {
+  [key in string]: {
+    ref: InputRefs;
+  };
+};
+
+export function useForm() {
+  const [state, setState] = useState<State>({});
+
+  const getFields = () => {
+    const values: Record<string, any> = {};
+
+    Object.keys(state).forEach((key) => {
+      values[key] = state[key].ref;
+    });
+  };
+
+  const init = (name: string) => {
+    const ref = createRef<HTMLInputElement>();
+
+    setState((prev) => ({
+      ...prev,
+      [name]: {
+        ...prev[name],
+        ref,
+      },
+    }));
+
+    return {
+      ref,
+    };
+  };
 
   return {
-    formState: {},
     init,
+    formState: { getFields },
   };
 }
